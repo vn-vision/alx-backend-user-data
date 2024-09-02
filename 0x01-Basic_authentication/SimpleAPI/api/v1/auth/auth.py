72473d9fc0a4de02e68030b5f0eb57d0109726b4
kZilla <kzilla@vn-z.zilla>
2024-09-02 12:29:33 +0300

    APIs Basic Auth

---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -0,0 +1,45 @@
+"""
+Class Auth: - manage API authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Returns
+        - True if:
+            path is not in the list of excluded paths
+            path is None
+            excluded paths list is None or empty
+        - False if:
+            path is in excluded paths list
+        """
+
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+
+        # remove all trailing / in paths in the excluded paths
+        excluded_paths = [path.rstrip('/') for path in excluded_paths]
+        path = path.rstrip('/')
+
+        return path not in excluded_paths
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Returns None if request is None
+            request doesn't contain the header key Authorization
+        return value of the header request Authorization
+        """
+        if request is None:
+            return None
+
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Returns None
+        """
+        return None
---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    Handles basic authentication
+    """
+    pass
